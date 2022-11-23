2cfef49cde3667a6807510e31c0ebc330a997301
Natallia <novikn1987@gmail.com>
2022-11-24 00:06:15 +0300

    +++

--- academy/src/by/academy/lesson2/classwork/Application.java
+++ academy/src/by/academy/lesson2/classwork/Application.java
@@ -2,6 +2,17 @@ package by.academy.lesson2.classwork;
 
 public class Application {
 	public static void main (String [] args) {
-
+		
+Cat cat1= new Cat();
+Cat cat2= new Cat("Shuher");
+cat2.eat();
+cat2.sleep();
+cat2.walk();
+cat1.grow();
+cat1.grow();
+cat1.grow();
+System.out.println(cat1.age);
+System.out.println(cat2.getMoneystatus());
+cat1.setMoney(5);
 }
 }
