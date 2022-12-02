c941b006846279535a90a05945166dbf2a287d38
Natallia <novikn1987@gmail.com>
2022-12-03 00:28:18 +0300

    homework1,2

---
+++ academy/src/by/academy/homework1/Homework1_1.java
@@ -0,0 +1,45 @@
+package by.academy.homework1;
+
+import java.util.Scanner;
+
+public class Homework1_1 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите сумму покупки: ");
+		int order = sc.nextInt();
+
+		System.out.println("Введите возраст покупателя: ");
+		int age = sc.nextInt();
+
+		if (order < 100)
+			System.out.println(String.format("Ваша скидка 5 процентов, к оплате %s", ((double) order * 0.95)));
+		if (order >= 100 & order < 200)
+
+			System.out.println(String.format("Ваша скидка 7 процентов, к оплате %s", ((double) order * 0.93)));
+
+		if (order >= 200 && order < 300 && age > 18)
+
+			System.out.println(String.format("Ваша скидка 16 процентов, к оплате %s", ((double) order * 0.84)));
+		else
+			System.out.println(String.format("Ваша скидка 9 процентов, к оплате %s", ((double) order * 0.91)));
+		if (order >= 300 & order < 400)
+
+			System.out.println(String.format("Ваша скидка 15 процентов, к оплате %s", ((double) order * 0.85)));
+
+		if (order >= 400)
+
+			System.out.println(String.format("Ваша скидка 20 процентов, к оплате %s", ((double) order * 0.80)));
+
+		
+		
+		sc.close();
+	
+	
+	
+	
+	
+	
+	
+	}
+}
---
+++ academy/src/by/academy/homework1/Homework1_2.java
@@ -0,0 +1,45 @@
+package by.academy.homework1;
+
+import java.util.Scanner;
+
+public class Homework1_2 {
+	public static void main(String[] args) {
+
+		Scanner console = new Scanner(System.in); // ctrl+shift+O на англ раскладке, чтобы появился Import сканнера
+		System.out.println("Введите тип данных");
+		String dataType = console.nextLine().toLowerCase();	//снижает регистр букв
+
+		switch (dataType) {
+		case "int":{
+			System.out.println("Введите переменную");
+			int variable = console.nextInt();
+			System.out.println(String.format("Остаток от деления на 2 = %s", variable%2 ));
+			break;}
+		case "double":{
+			System.out.println("Введите переменную");
+			double variable = console.nextDouble();			
+			System.out.println(String.format("70 процентов = %s", (variable)*0.7));
+			break;}
+		case "float":{
+			System.out.println("Введите переменную");
+			float variable = console.nextFloat();
+			System.out.println(String.format("Квадрат числа = %s", variable*variable));
+			break;}
+		case "char":{
+			System.out.println("Введите переменную");
+			String variable = console.next();			
+			System.out.println(String.format("charAt 0 %s", variable.charAt(0)));
+			break;}
+		case "string":{
+			System.out.println("Введите переменную");
+			String variable = console.next();
+			System.out.println(String.format("Hello %s", variable));
+			break;}
+		default: System.out.println("Unsupported type");	
+		
+		
+		console.close();
+		}
+		
+	}
+}
