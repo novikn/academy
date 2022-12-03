afc12434c2cf417d8458697a546c40fe45259c90
Natallia <novikn1987@gmail.com>
2022-12-03 04:42:09 +0300

    Homework1_4

---
+++ academy/src/by/academy/homework1/Homework1_4.java
@@ -0,0 +1,13 @@
+package by.academy.homework1;
+
+public class Homework1_4 {
+    public static void main(String[] args) {
+        int n = 2;
+        int result=2;
+        do {
+            result =result*2;
+            System.out.println("2 в степени " + n + "=" + result);
+            n++;
+        } while (result < 1000000);
+    }
+}
